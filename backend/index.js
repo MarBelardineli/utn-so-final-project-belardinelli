523a09c34d7d82781be3878a2a65c2c561f1a2ee
marbe <belardinelli.marianela@gmail.com>
2025-07-09 10:24:38 -0300

    Correcciones finales

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const db = require("./db");
 
 // Define express app
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -18,6 +18,27 @@ app.get("/api/students", async (req, res) => {
     console.error(err);
     res.status(500).send("DB error");
   }
+});
+
+    // Saludar 
+app.get("/api/greet/:name", (req,res) => {
+  const name = req.params.name;
+  res.json({ message: `¡Hola ${name}!`})
+  })
+
+    //Agregar estudiante a DB
+app.post("/api/students", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "El campo 'name' es requerido." });
+  }
+  const result = await db.query(
+    "INSERT INTO students (name) VALUES ($1) RETURNING id, name",
+    [name]
+  );
+
+  res.status(201).json(result.rows[0]);
 });
 
 // Start the server
--- frontend/script.js
+++ frontend/script.js
@@ -9,3 +9,53 @@ document.getElementById("loadButton").addEventListener("click", async () => {
     tbody.appendChild(row);
   });
 });
+
+document.getElementById("greetBtn").addEventListener("click", async () => {
+
+  const name = document.getElementById('nameInput').value;
+  const input = document.getElementById('nameInput'); 
+
+  if (!name) {
+    alert('Por favor, ingresá tu nombre');
+    return;
+  }
+  fetch(`/api/greet/${encodeURIComponent(name)}`)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('greetingMessage').textContent = data.message;
+    })
+  input.value = "";
+})
+
+document.getElementById("addStudent").addEventListener("submit", async (event) => {
+  
+  event.preventDefault(); 
+
+  const input = document.getElementById("newStudentName");
+  const name = input.value;
+
+  if (!name) {
+    alert('Por favor, ingresá un nombre');
+    return;
+  }
+
+  const response = await fetch("/api/students", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name }),
+  });
+
+  const newStudent = await response.json();
+  const status = document.getElementById("statusMessage");
+  status.textContent = `Estudiante agregado: ${newStudent.name} (ID ${newStudent.id})`;
+ 
+  console.log("Estudiante agregado:", newStudent);
+
+  input.value = "";
+
+  document.getElementById("loadButton").click();
+
+  
+})
